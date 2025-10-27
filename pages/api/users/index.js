c1deb71754b1e5cf66740ca85fd091937eb4f404
DanielG77 <danielgirgar@gmail.com>
2025-10-27 01:14:12 +0100

    Testeo notificacion y cambio index

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -14,7 +14,6 @@ function handler(req, res) {
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-
   function getUsers() {
     const users = usersRepo.getAll();
     return res.status(200).json(users);
